4d5ac55bbc491bb91e4422d43e956eb2c77f790a
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-09 14:23:00 +0300

    02:05 заготовка клиента

---
+++ Services/WebStore.Services/WebStore.WebAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,41 @@
+﻿using WebStore.Interfaces.TestAPI;
+namespace WebStore.WebAPI.Clients.Values
+{
+    public class ValuesClient : IValuesService
+    {
+
+        public ValuesClient()
+        {
+
+        }
+        public void Add(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int id, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
