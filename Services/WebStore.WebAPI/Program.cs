4c0d29f1ae5d409559f9b65faec1020196319185
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-10 18:45:22 +0300

    Подключение необходимых сервисов для ВебАПИ

--- Services/WebStore.WebAPI/Program.cs
+++ Services/WebStore.WebAPI/Program.cs
@@ -1,15 +1,82 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces.Services;
+using WebStore.Services.InMemory;
+using WebStore.Services.InSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var services = builder.Services;
+var configuration = builder.Configuration;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+#region Настройка сервисов приложения
+
+var db_connection_string_name = configuration["Database"];
+var db_connection_string = configuration.GetConnectionString(db_connection_string_name);
+switch (db_connection_string_name)
+{
+    case "SqlServer":
+    case "DockerDB":
+        services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(db_connection_string));
+        break;
+
+    case "Sqlite":
+        services.AddDbContext<WebStoreDB>(opt => opt.UseSqlite(db_connection_string, o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
+        break;
+}
+
+services.AddIdentity<User, Role>(/*opt => opt.*/)
+   .AddEntityFrameworkStores<WebStoreDB>()
+   .AddDefaultTokenProviders();
+
+services.Configure<IdentityOptions>(opt =>
+{
+#if DEBUG
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 3;
+    opt.Password.RequiredUniqueChars = 3;
+#endif
+
+    opt.User.RequireUniqueEmail = false;
+    opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ1234567890";
+
+    opt.Lockout.AllowedForNewUsers = false;
+    opt.Lockout.MaxFailedAccessAttempts = 10;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+});
+
+services.ConfigureApplicationCookie(opt =>
+{
+    opt.Cookie.Name = "WebStore.GB";
+    opt.Cookie.HttpOnly = true;
+
+    opt.ExpireTimeSpan = TimeSpan.FromDays(10);
+
+    opt.LoginPath = "/Account/Login";
+    opt.LogoutPath = "/Account/Logout";
+    opt.AccessDeniedPath = "/Account/AccessDenied";
+
+    opt.SlidingExpiration = true;
+});
+
+
+services.AddScoped<IEmployeesData, InMemoryEmployeesData>();
+services.AddScoped<IProductData, SqlProductData>();
+services.AddScoped<IOrderService, SqlOrderService>();
+
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(); 
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
