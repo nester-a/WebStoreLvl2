e7f4e3fddb12d7b1c2ffa6d5fadeb5cb91ef0427
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-10 18:57:33 +0300

    Заготовка клиента

---
+++ Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Employees
+{
+    internal class EmployeesClient : BaseClient, IEmployeesData
+    {
+        private readonly ILogger<EmployeesClient> logger;
+
+        public EmployeesClient(HttpClient client, ILogger<EmployeesClient> logger) : base(client, "api/employees")
+        {
+            this.logger = logger;
+        }
+
+        public int Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
