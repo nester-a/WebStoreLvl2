7bd230f2fd70ce92e1e2e71c39a2e8ae085a5baa
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-13 22:03:19 +0300

    добавление ДТО секции

---
+++ Common/WebStore.DTO/SectionDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStore.DTO
+{
+    public class SectionDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Order { get; set; }
+
+        public List<SectionDTO> ChildSections { get; set; } = new();
+    }
+}
