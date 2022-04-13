a06dc802ece42da4e21c8a6250d53840aa053cba
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-13 22:12:47 +0300

    Добавление продуктмаппера

---
+++ Services/WebStore.Mappers/ProductMapper.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using WebStore.Domain.Entities;
+using WebStore.DTO;
+using WebStore.ViewModels;
+
+namespace WebStore.Mappers
+{
+    public class ProductMapper
+    {
+
+        public static ProductViewModel DTOToViewModel(ProductDTO dto)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<ProductDTO, ProductViewModel>());
+            var mapper = new Mapper(config);
+
+            var model = mapper.Map<ProductViewModel>(dto);
+            return model;
+        }
+        public static ProductDTO ViewModelToDTO(ProductViewModel model)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<ProductViewModel, ProductDTO>());
+            var mapper = new Mapper(config);
+
+            var dto = mapper.Map<ProductDTO>(model);
+            return dto;
+        }
+
+        public static ProductDTO EntityToDTO(Product employee)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductDTO>());
+            var mapper = new Mapper(config);
+
+            var dto = mapper.Map<EmployeeDTO>(employee);
+            return dto;
+        }
+
+        public static Product DTOToEntity(ProductDTO dto)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<ProductDTO, Product>());
+            var mapper = new Mapper(config);
+
+            var employee = mapper.Map<Product>(dto);
+            return employee;
+        }
+    }
+}
