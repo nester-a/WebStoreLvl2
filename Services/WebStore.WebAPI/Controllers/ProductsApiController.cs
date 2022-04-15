ad92466dc6dbb613b3abc953f81ee73b07c4856d
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-15 22:45:33 +0300

    Не работает фильтрация Возвращает всех хорошо, но не фильтрует нормально

--- Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
+++ Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Interfaces.Services;
+using WebStore.Mappers;
 
 namespace WebStore.WebAPI.Controllers
 {
@@ -6,5 +9,31 @@ namespace WebStore.WebAPI.Controllers
     [ApiController]
     public class ProductsApiController : ControllerBase
     {
+        private readonly IProductData products;
+        private readonly ILogger<ProductsApiController> logger;
+
+        public ProductsApiController(IProductData products, ILogger<ProductsApiController> logger)
+        {
+            this.products = products;
+            this.logger = logger;
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetBrandById(int Id)
+        {
+            var brand = products.GetBrandById(Id);
+            if (brand is null)
+                return NoContent();
+
+            var dto = BrandMapper.EntityToDTO(brand);
+            return Ok(dto);
+        }
+        [HttpPost()]
+        public IActionResult GetProducts([FromBody]ProductFilter? Filer = null)
+        {
+            var entitys = products.GetProducts(Filer);
+            var result = entitys.Select(p => ProductMapper.EntityToDTO(p));
+            return Ok(result);
+        }
     }
 }
