0c53e66f173588467f5428ee4f4a1a6f47e54cb0
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-16 11:51:04 +0300

    выделение ДТО связанных с заказами в отдельные документы

---
+++ Common/WebStore.DTO/CreateOrderDTO.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.ViewModels;
+
+namespace WebStore.DTO
+{
+    public class CreateOrderDTO
+    {
+        public OrderViewModel Order { get; set; } = null;
+
+        public IEnumerable<OrderItemDTO> Items { get; set; } = null;
+    }
+}
--- Common/WebStore.DTO/OrderDTO.cs
+++ Common/WebStore.DTO/OrderDTO.cs
@@ -1,6 +1,4 @@
-﻿using WebStore.ViewModels;
-
-namespace WebStore.DTO
+﻿namespace WebStore.DTO
 {
     public class OrderDTO
     {
@@ -9,19 +7,6 @@ namespace WebStore.DTO
         public string Address { get; set; } = null;
         public string? Description { get; set; }
         public DateTimeOffset Date { get; set; }
-        public IEnumerable<OrderItemDTO> Items { get; set; } = null;
-    }
-    public class OrderItemDTO
-    {
-        public int Id { get; set; }
-        public int ProductId { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
-    }
-    public class CreateOrderDTO
-    {
-        public OrderViewModel Order { get; set; } = null;
-
         public IEnumerable<OrderItemDTO> Items { get; set; } = null;
     }
 }
