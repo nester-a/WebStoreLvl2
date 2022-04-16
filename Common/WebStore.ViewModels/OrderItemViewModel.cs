86ea8fe15f82d395bb5fc5ab879803b8d7248a01
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-16 11:46:10 +0300

    модель представление ордер айтема

---
+++ Common/WebStore.ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebStore.ViewModels
+{
+    public class OrderItemViewModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
