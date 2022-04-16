c44903ece7cc46c38a43500897da31d8c2c3fab7
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-16 11:29:46 +0300

    заготовка клиента заказов

---
+++ Services/WebStore.WebAPI.Clients/Orders/OrderClient.cs
@@ -0,0 +1,29 @@
+﻿using WebStore.Domain.Entities.Orders;
+using WebStore.Interfaces.Services;
+using WebStore.ViewModels;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Orders
+{
+    public class OrderClient : BaseClient, IOrderService
+    {
+        public OrderClient(HttpClient client) : base(client, "api/orders")
+        {
+        }
+
+        public Task<Order> CreateOrderAsync(string UserName, CartViewModel Cart, OrderViewModel OrderModel, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order?> GetOrderByIdAsync(int Id, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Order>> GetUserOrdersAsync(string UserName, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
