4e02a7a5852d77ce17e7522a88af6ffb46725a39
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-16 11:37:41 +0300

    ДТО модель заказа

---
+++ Common/WebStore.DTO/OrderDTO.cs
@@ -0,0 +1,27 @@
+﻿using WebStore.ViewModels;
+
+namespace WebStore.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public string Phone { get; set; } = null;
+        public string Address { get; set; } = null;
+        public string? Description { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public IEnumerable<OrderItemDTO> Items { get; set; } = null;
+    }
+    public class OrderItemDTO
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class CreateOrderDTO
+    {
+        public OrderViewModel Order { get; set; } = null;
+
+        public IEnumerable<OrderItemDTO> Items { get; set; } = null;
+    }
+}
