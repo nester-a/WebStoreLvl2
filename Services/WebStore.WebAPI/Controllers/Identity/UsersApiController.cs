e4f96f74948c1ac9c1633e15e62b55becce325a1
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-17 14:32:18 +0300

    Заготовки контроллеров вебАПИ системы идентити

---
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersApiController : ControllerBase
+    {
+    }
+}
