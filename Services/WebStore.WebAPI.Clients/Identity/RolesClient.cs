4ef2f8d253c9e2211a2d59f91100cf947d07d9a7
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-17 18:01:08 +0300

    Заготовка клиентов вебАПИ системы идентити

---
+++ Services/WebStore.WebAPI.Clients/Identity/RolesClient.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Interfaces;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Identity;
+public class RolesClient : BaseClient
+{
+    public RolesClient(HttpClient client) : base(client, WebAPIAddresses.V1.Identity.Roles)
+    {
+    }
+
+}
---
+++ Services/WebStore.WebAPI.Clients/Identity/UsersClient.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Interfaces;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Identity;
+
+public class UsersClient : BaseClient
+{
+    public UsersClient(HttpClient client) : base(client, WebAPIAddresses.V1.Identity.Users)
+    {
+    }
+}
