d6da71b92a9bc4d33df4d70d3960b70cc99acd42
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-17 17:32:58 +0300

    ДТО модели для системы идентити

---
+++ Common/WebStore.DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace WebStore.DTO.Identity;
+public class ClaimDTO : UserDTO
+{
+    public IEnumerable<Claim> Claims { get; set; } = null;
+}
+
+public class ReplaceClaimDTO : UserDTO
+{
+    public Claim Claim { get; set; } = null;
+    public Claim NewClaim { get; set; } = null;
+}
