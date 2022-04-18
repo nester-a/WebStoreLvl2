87e78ce51946649faca3ce248b0a25d506f33030
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-18 22:53:19 +0300

    Провайдер ситемы логирования лог4нет

---
+++ Common/WebStore.Logging/Log4NetLoggerProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using System.Xml;
+
+namespace WebStore.Logging;
+
+public class Log4NetLoggerProvider : ILoggerProvider
+{
+    private readonly string configurationFile;
+    private readonly ConcurrentDictionary<string, Log4NetLogger> loggers = new();
+
+    public Log4NetLoggerProvider(string ConfigurationFile)
+    {
+        configurationFile = ConfigurationFile;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return loggers.GetOrAdd(categoryName, static (category, config) =>
+        {
+            var xml = new XmlDocument();
+            xml.Load(config);
+            return new Log4NetLogger(category, xml["log4net"]!);
+        }, configurationFile);
+    }
+
+    public void Dispose()
+    {
+        var oldLoggers = loggers.Values.ToArray();
+        loggers.Clear();
+        foreach (var log in oldLoggers)
+        {
+            log.Dispose();
+        }
+    }
+}
