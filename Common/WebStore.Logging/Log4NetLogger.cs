c3642ff234f9831d235120d83edc9a69466a537b
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-18 22:58:36 +0300

    Делаем класс логгер Урок 1:31

--- Common/WebStore.Logging/Log4NetLogger.cs
+++ Common/WebStore.Logging/Log4NetLogger.cs
@@ -1,16 +1,41 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using log4net;
+using log4net.Config;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using System.Xml;
 
 namespace WebStore.Logging;
 
-public class Log4NetLoggerProvider : ILoggerProvider
+public class Log4NetLogger : ILogger, IDisposable
 {
-    public Log4NetLoggerProvider(string ConfigurationFile)
+    private readonly ILog _log;
+
+    public Log4NetLogger(string category, XmlElement configuration)
     {
+        var loggerRepos = LogManager
+            .CreateRepository(
+                Assembly.GetEntryAssembly(),
+                typeof(log4net.Repository.Hierarchy.Hierarchy));
 
+        _log = LogManager.GetLogger(loggerRepos.Name, category);
+
+        XmlConfigurator.Configure(configuration);
     }
-}
 
-public class Log4NetLogger
-{
+    public IDisposable BeginScope<TState>(TState state) => null!;
 
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        throw new NotImplementedException();
+    }
 }
